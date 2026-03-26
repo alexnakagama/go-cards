9242975f4826f59c001716d1e94d2c1972fe0a3f
alex <alexnakagama4@gmail.com>
2026-03-26 03:06:00 -0300

    added comments explaining the functions

--- deck.go
+++ deck.go
@@ -8,19 +8,26 @@ import (
 
 type Deck []string
 
+// Creates a new deck of cards
 func newDeck() Deck {
+	// Creates a deck slice
 	cards := Deck{}
+	// Creates cardSuits which is a slice of string
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Club"}
+	// Creates cardValues which is a slice of string
 	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"}
 
+	// Use of a for loop to create the cards and append them to the cards deck
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
 		}
 	}
+	// returns a new deck
 	return cards
 }
 
+// Prints with a receiver function
 func (d Deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
@@ -43,6 +50,8 @@ func (d Deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
+// Function to read a file, if it is null exits the program with os.Exit(1)
+// strings.Split returns a new slice with a separator of a ","
 func newDeckFromFile(filename string) Deck {
 	bs, err := os.ReadFile(filename)
 	if err != nil {
--- main.go
+++ main.go
@@ -3,5 +3,4 @@ package main
 func main() {
 	cards := newDeckFromFile("my_cards")
 	cards.print()
-
 }
